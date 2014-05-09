7789c7a1b1a0e97e7066c5222b5f8615d62d5dfb
Tu Vo <tu.vo@navy.mil>
2014-05-09 16:07:58 -0400

    friday updates

--- src/MyTelnet.java
+++ src/MyTelnet.java
@@ -15,7 +15,7 @@ public class MyTelnet {
 
 		System.out.println("Tu's MyClient.\n");
 		/* printLocalAddress (); */
-		System.out.println("Using server: " + serverName + ", Port: 2540");
+		System.out.println("Using server: " + serverName + ", Port:" + port);
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		try {
 			String name;
@@ -25,7 +25,7 @@ public class MyTelnet {
 				System.out.flush();
 				//System.out.flush();
 				name = in.readLine();
-				//host = in.readLine();
+				host = in.readLine();
 				if (name.indexOf("quit") < 0)
 					printMessageToServer(name, serverName , port , host);
 			} while (name.indexOf("quit") < 0);
@@ -40,10 +40,10 @@ public class MyTelnet {
 		BufferedReader fromServer;
 		PrintStream toServer;
 		String textFromServer;
-		
+
 		String outputData =
-				  "GET /elliott/dog.txt HTTP/1.1 /" +		  
-				  "Host: " + "condor.depaul.edu:80" + "\r\n" +
+				  ""+ name + "\r\n" +		  
+				  "Host: " + serverName + "\r\n" +
 				  "Connection: keep-alive" + "\r\n" +
 				  "\r\n\r\n";
 
@@ -57,24 +57,21 @@ public class MyTelnet {
 			fromServer = new BufferedReader(new InputStreamReader(
 					sock.getInputStream()));
 			toServer = new PrintStream(sock.getOutputStream());
-								
-			// Send machine name or IP address to server:
-			/*toServer.append(name + "\n");
-			toServer.append(host + "\n");
-			toServer.append("connection: keep-alive \n");
-			toServer.append("Date: " + new Date() + "\n");
-			toServer.append("\n\n");
-			toServer.flush();*/
-			
-			toServer.println(name);
+
+			toServer.println(outputData);
 			toServer.flush();
 
 			// Read two or three lines of response from the server,
 			// and block while synchronously waiting:
-			for (int i = 1; i <= 10; i++) {
+			boolean streamEnd = false;
+			while (streamEnd ==false) {
 				textFromServer = fromServer.readLine();
-				if (textFromServer != null)
+				if (textFromServer != null){
 					System.out.println(textFromServer);
+					}
+				else
+					streamEnd = true;
+
 			}
 
 			sock.close();
---
+++ src/MyWebServer.java
@@ -0,0 +1,138 @@
+import java.io.*; // Get the Input Output libraries 
+import java.net.*;
+import java.util.Date;
+
+// Get the Java networking libraries 
+
+class webWorker extends Thread { // Class definition
+	Socket sock; // Class member, socket, local to webwebwebWorker.
+
+	webWorker(Socket s) {
+		sock = s;
+	} // Constructor, assign arg s to local sock
+
+	public void run() {
+		// Get I/O streams from the socket:
+		PrintStream out = null;
+		BufferedReader in = null;
+		
+		try {
+			out = new PrintStream(sock.getOutputStream());
+			in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+			PrintStream pout = new PrintStream(out);
+
+			// read first line of request (ignore the rest)
+			String request = in.readLine();
+
+			// parse the line
+			if (!request.startsWith("GET")
+					|| request.length() < 14
+					|| !(request.endsWith("HTTP/1.0") || request
+							.endsWith("HTTP/1.1"))) {
+				// bad request
+				errorReport(pout, sock, "400", "Bad Request",
+						"Your browser sent a request that "
+								+ "this server could not understand.");
+			} else {
+				String req = request.substring(4, request.length() - 9).trim();
+				String req2 = request.substring(5, request.length() - 9).trim();
+				if (req.indexOf("..") != -1 || req.indexOf("/.ht") != -1
+						|| req.endsWith("~")) {
+					// evil hacker trying to read non-wwwhome or secret file
+					errorReport(pout, sock, "403", "Forbidden",
+							"You don't have permission to access the requested URL.");
+				}else{
+					File f = new File(req2);
+					InputStream file = new FileInputStream(f);
+
+					String outputData = "HTTP/1.1 200 OK" + "\r\n"
+							+ "Content-Type: " + contentType(req) + "\r\n"
+							+ "Content-Lenght: : " + file.available() + "\r\n" 
+							+ "\r\n\r\n";
+
+					out.println(outputData);
+					System.out.println(outputData);
+
+					sendFileToClient(file, out); // send raw file
+					log(sock, "200 OK");
+					out.flush();
+					sock.close();
+				}
+			}
+
+		} catch (IOException ioe) {
+			System.out.println(ioe);
+		}
+
+	}
+
+	private static void log(Socket sock, String msg) {
+		System.err.println(new Date() + " ["
+				+ sock.getInetAddress().getHostAddress() + ":" + sock.getPort()
+				+ "] " + msg);
+	}
+
+	static void returnMimeToBrowser(String name, PrintStream out) {
+		out.println(name);
+
+	}
+
+	private static String contentType(String path) {
+		if (path.endsWith(".html") || path.endsWith(".htm"))
+			return "text/html";
+		else if (path.endsWith(".txt") || path.endsWith(".java"))
+			return "text/plain";
+		else if (path.endsWith(".gif"))
+			return "image/gif";
+		else if (path.endsWith(".class"))
+			return "application/octet-stream";
+		else if (path.endsWith(".jpg") || path.endsWith(".jpeg"))
+			return "image/jpeg";
+		else
+			return "text/plain";
+	}
+
+	private static void sendFileToClient(InputStream file, OutputStream out) {
+		try {
+			byte[] buffer = new byte[1000];
+			while (file.available() > 0)
+				out.write(buffer, 0, file.read(buffer));
+		} catch (IOException e) {
+			System.err.println(e);
+		}
+	}
+
+	private static void errorReport(PrintStream pout, Socket sock, String code,
+			String title, String msg) {
+		pout.print("HTTP/1.0 " + code + " " + title + "\r\n" + "\r\n"
+				+ "<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML 2.0//EN\">\r\n"
+				+ "<TITLE>" + code + " " + title + "</TITLE>\r\n"
+				+ "</HEAD><BODY>\r\n" + "<H1>" + title + "</H1>\r\n" + msg
+				+ "<P>\r\n" + "<HR><ADDRESS>FileServer 1.0 at "
+				+ sock.getLocalAddress().getHostName() + " Port "
+				+ sock.getLocalPort() + "</ADDRESS>\r\n" + "</BODY></HTML>\r\n");
+		log(sock, code + " " + title);
+	}
+
+}
+
+public class MyWebServer {
+
+	public static boolean controlSwitch = true;
+
+	public static void main(String args[]) throws IOException {
+		int q_len = 6; /* Number of requests for OpSys to queue */
+		int port = 2540;
+		Socket sock;
+
+		ServerSocket servsock = new ServerSocket(port, q_len);
+		System.out.println("Tu's server starting up, listening at port .\n" + port);
+		while (controlSwitch) {
+			// wait for the next client sock:
+			sock = servsock.accept();
+			new webWorker(sock).start(); // Uncomment to see shutdown bug:
+			// try{Thread.sleep(10000);} catch(InterruptedException ex) {}
+		}
+
+	}
+}
